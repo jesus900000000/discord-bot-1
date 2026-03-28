b19023e759ac0dc7a5f72532695e1b7ea1ae484f
jesus900000000 <jgutierrez9000@gmail.com>
2026-03-28 00:00:42 -0500

    initial commit

---
+++ yapper.py
@@ -0,0 +1,20 @@
+import requests
+import time
+import os
+
+payload = {
+    'content': str(int(time.time() * 1000))
+}
+
+
+header = {
+    'authorization': os.environ["AUTH"]
+}
+
+
+while True:
+    r = requests.post('https://discord.com/api/v9/channels/1486199330795688107/messages', data=payload, headers=header)
+    time.sleep(2)
+    payload = {
+        'content': str(int(time.time() * 1000))
+    }
