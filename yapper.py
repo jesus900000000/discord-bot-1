6174f964facf83fe76d06043600d3d242254dcd8
jesus900000000 <jgutierrez9000@gmail.com>
2026-03-30 18:53:40 -0500

    +catch network exception make more ressilient

--- yapper.py
+++ yapper.py
@@ -2,6 +2,9 @@ import requests
 import time
 import os
 
+#Variables:
+url = 'https://discord.com/api/v9/channels/1486199330795688107/messages'
+
 payload = {
     'content': str(int(time.time() * 1000))
 }
@@ -13,8 +16,14 @@ header = {
 
 
 while True:
-    r = requests.post('https://discord.com/api/v9/channels/1486199330795688107/messages', data=payload, headers=header)
-    time.sleep(2)
-    payload = {
+    try:
+        r = requests.post(url, data=payload, headers=header,timeout=10)
+        print("Sent:",r.status_code)
+        payload = {
         'content': str(int(time.time() * 1000))
-    }
+        }
+    except requests.exceptions.RequestException as e:
+        print("Network error", e)
+        time.sleep(5)
+        continue
+    time.sleep(2)
