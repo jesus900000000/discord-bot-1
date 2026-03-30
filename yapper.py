e5f7a9cbb29ef448c3f85e1dc055bf037543af3e
jesus900000000 <jgutierrez9000@gmail.com>
2026-03-30 01:54:57 -0500

    Revert to working order. no t. delete integers.

--- yapper.py
+++ yapper.py
@@ -16,5 +16,5 @@ while True:
     r = requests.post('https://discord.com/api/v9/channels/1486199330795688107/messages', data=payload, headers=header)
     time.sleep(2)
     payload = {
-        'content': str(int(time.time() * 1000)) + "t"
+        'content': str(int(time.time() * 1000))
     }
